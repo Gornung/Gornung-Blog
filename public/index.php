77a9ca5d6bf05f2db7b80b89f6542ad1af4c6098
sWIFalgorn <alexander.gornung@stud.fh-rosenheim.de>
2021-04-14 14:51:35 +0200

    Übung 1

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Hello world!';
+declare(strict_types=1);
+
+use Gornung\Webentwicklung\Request;
+use Gornung\Webentwicklung\Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$urlPath = $_SERVER['REQUEST_URI'];
+
+if (strpos(strtolower($urlPath), 'hello')) {
+    $personToGreet = 'Stranger';
+    if (isset($_REQUEST['name'])) {
+        $personToGreet = $_REQUEST['name'];
+    }
+    echo 'Hello ' . $personToGreet;
+} else {
+    echo 'Hello there!';
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit038ee05dd05a08bce118049ae88c4be0
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit038ee05dd05a08bce118049ae88c4be0', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit038ee05dd05a08bce118049ae88c4be0', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit038ee05dd05a08bce118049ae88c4be0::getInitializer($loader));
         } else {
